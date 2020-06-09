11d028a72d447e5eaf4ef0763ca5213f11dcab28
Rozafë Llalloshi <rozafallalloshi@gmail.com>
2020-06-09 16:17:42 +0200

    code fixes

--- scripts/visualize_scene_objects.py
+++ scripts/visualize_scene_objects.py
@@ -5,16 +5,18 @@ import open3d as o3d
 
 from lib.config import CONF
 
-def numpy_to_point_cloud(object):
+
+def numpy_to_point_cloud(scene_object):
     pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(object[:, 0:3])
-    pcd.colors = o3d.utility.Vector3dVector(object[:, 3:6])
+    pcd.points = o3d.utility.Vector3dVector(scene_object[:, 0:3])
+    pcd.colors = o3d.utility.Vector3dVector(scene_object[:, 3:6])
     return pcd
 
-def visualize_scene_objects(args):
-    print(args)
+
+def visualize_scene_objects(scene_id):
+    print(scene_id)
     scene_objects = np.load(
-        os.path.join(CONF.PATH.SCANNET_DATA, args.scene_id) + "_objects.npy", allow_pickle=True)
+        os.path.join(CONF.PATH.SCANNET_DATA, scene_id) + "_objects.npy", allow_pickle=True)
     print(scene_objects.shape)
     num_objects = scene_objects.shape[0]
     for i in range(num_objects):
@@ -28,4 +30,4 @@ if __name__ == "__main__":
                         required=True)
     args = parser.parse_args()
 
-    visualize_scene_objects(args)
+    visualize_scene_objects(args.scene_id)
