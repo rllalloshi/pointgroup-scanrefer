4fa11089e2800407bba332968357f9629d6fc1dd
Rozafë Llalloshi <rozafallalloshi@gmail.com>
2020-06-19 17:14:08 +0200

    read pointgroup config path from base path

--- lib/util/config.py
+++ lib/util/config.py
@@ -7,9 +7,12 @@ import argparse
 import yaml
 import os
 
+from lib.config import CONF
+
+path = os.path.join(CONF.PATH.BASE, 'config/pointgroup_run1_scannet.yaml')
 def get_parser():
     args = argparse.ArgumentParser(description='Point Cloud Segmentation')
-    with open('/datadrive/forked/NEW/config/pointgroup_run1_scannet.yaml', 'r') as f:
+    with open(path, 'r') as f:
         config = yaml.load(f)
     for key in config:
         for k, v in config[key].items():
@@ -19,4 +22,4 @@ def get_parser():
 
 
 cfg = get_parser()
-setattr(cfg, 'exp_path', os.path.join('exp', cfg.dataset, cfg.model_name, '/datadrive/forked/NEW/config/pointgroup_run1_scannet.yaml'.split('/')[-1][:-5]))
+setattr(cfg, 'exp_path', os.path.join('exp', cfg.dataset, cfg.model_name, path.split('/')[-1][:-5]))
